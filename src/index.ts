9d7f7ceb050100fed35a82fa989b6af6a0c01ad1
shouta0715 <Myheroes0715@gmail.com>
2024-05-19 14:55:15 +0900

    add: prettier eslint durable objects

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { createMiddleware } from "hono/factory";
+import { SyncEnv } from "@/types/env";
 
-const app = new Hono()
+const app = new Hono<SyncEnv>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const sessionMiddleware = createMiddleware<SyncEnv>(async (c, next) => {
+  const upgradeHeader = c.req.header("Upgrade");
 
-export default app
+  if (!upgradeHeader || upgradeHeader !== "websocket") {
+    return next();
+  }
+
+  const session = c.env.SESSION.idFromName("session");
+
+  const stub = c.env.SESSION.get(session);
+
+  c.set("session", stub);
+
+  return next();
+});
+
+app.get("/", sessionMiddleware, async (c) => {
+  return c.var.session.fetch(c.req.raw);
+});
+
+export { WebMultiViewSync } from "@/sync";
+export { WebMultiViewSession } from "@/session";
+export default app;
